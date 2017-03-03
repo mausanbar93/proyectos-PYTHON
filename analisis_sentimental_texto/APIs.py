ab3e7b3610dc40db7234ea36634765d67dcacb3a
Mauricio Sanchez Barragan <mauricio.sanchez@seratic.com>
2017-03-03 17:50:33 -0500

    Lógica API sentiment
    Se agrega el analisis de texto con la API sentiment

--- analisis_sentimental_texto/APIs.py
+++ analisis_sentimental_texto/APIs.py
@@ -4,7 +4,7 @@ import unirest,json
 #Definimos clase consultas, que va a contener las peticiones a las APIs
 class consultas:
 
-	# Función que hace consultas a la API text-processing, análisis en inglés
+	# Función que hace consultas a la API text_processing, análisis en inglés
 	def text_processing(self,data):
 		response = unirest.post("http://text-processing.com/api/sentiment/",
 			headers={
@@ -20,7 +20,7 @@ class consultas:
 		resp = response.body['probability']
 		minimo = min(resp)
 		maximo = max(resp)
-		print '/********** API text-processing **********/'
+		print '/********** API text_processing **********/'
 		print 'Respuesta JSON: ',resp
 		print 'Porc Min: ',minimo,'',resp[minimo]
 		print 'Porc Max: ',maximo,'',resp[maximo]
@@ -31,7 +31,32 @@ class consultas:
 		else:
 			respuesta = 'Emocion Positiva'
 		print 'Respuesta Analisis: ',respuesta
-		print '/********** FIN API text-processing **********/\n'
+		print '/********** FIN API text_processing **********/\n'
 		#respuesta = json.dumps({maximo:resp[maximo]})
 
 		return respuesta
+
+	# Función que hace consultas a la API sentiment, análisis en inglés
+	def sentiment(self,data):
+		response = unirest.post("http://sentiment.vivekn.com/api/text/",
+			params={
+				"language": "auto",
+				"txt":data
+			}
+		)
+		resp = response.body['result']
+		emocion = resp['sentiment']
+		porcentaje = resp['confidence']
+		print '/********** API sentiment **********/'
+		print resp
+		print 'Porc: ',emocion,'',porcentaje
+		if emocion == 'Negative':
+			respuesta = 'Emocion Negativa'
+		elif emocion == 'Neutral':
+			respuesta = 'Emocion Neutral'
+		else:
+			respuesta = 'Emocion Positiva'
+		print 'Respuesta Analisis: ',respuesta
+		print '/********** FIN API sentiment **********/\n'
+
+		return respuesta;
